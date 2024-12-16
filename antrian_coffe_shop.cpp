ab9aa1e2ad2741136d90c7018bcdbab61c6b72fa
DavidCatch <raihanwibyanto@gmail.com>
2024-12-16 12:02:00 +0700

    Add DetailPesanan

--- antrian_coffe_shop.cpp
+++ antrian_coffe_shop.cpp
@@ -15,6 +15,28 @@ struct Customer {
 queue<Customer> customerQueue; 
 int queueCounter = 1; 
 
+void showOrderDetails() {
+    if (customerQueue.empty()) {
+        cout << "Tidak ada pelanggan dalam antrian.\n";
+        return;
+    }
+    Customer& customer = customerQueue.front();
+    cout << "\nDetail Pesanan untuk Pelanggan " << customer.name << " (No. Antrian " << customer.queueNumber << "):\n";
+    if (customer.orders.empty()) {
+        cout << "Belum ada pesanan.\n";
+    } else {
+        double total = 0;
+        cout << left << setw(15) << "Produk" << setw(10) << "Jumlah" << "Harga Total" << endl;
+        cout << "-------------------------------------\n";
+        for (map<string, int>::const_iterator it = customer.orders.begin(); it != customer.orders.end(); ++it) {
+            double subtotal = it->second * menu[it->first];
+            total += subtotal;
+            cout << left << setw(15) << it->first << setw(10) << it->second << "Rp " << subtotal << endl;
+        }
+        cout << "-------------------------------------\n";
+        cout << "Total Pembayaran: Rp " << total << endl;
+    }
+}
 
 void processPayment() {
     if (customerQueue.empty()) {
